3250df5265a321741db5a4011204ebd67dff329e
eziquexx <waftyann@gmail.com>
2024-10-04 18:32:01 +0900

    project 초기화

---
+++ src/main/java/com/ys/project1/ex/sbb/YsProject1ExCandySbbApplication.java
@@ -0,0 +1,13 @@
+package com.ys.project1.ex.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsProject1ExCandySbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsProject1ExCandySbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ys/project1/ex/candy/YsProject1ExCandySbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.project1.ex.candy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsProject1ExCandySbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
